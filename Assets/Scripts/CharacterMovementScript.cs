391d39fc66146be2c5777876536b6789d95a52fb
Nick Mudry <nick@mudry.me>
2015-04-17 23:21:04 -0400

    Basic Movement

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementScript : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.D)) //Move Right
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(speed, 0f));
+        }
+        if (Input.GetKey(KeyCode.A)) //Move Left
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(speed * -1f, 0f));
+        }
+        if (Input.GetKey(KeyCode.W)) //Move Up
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, speed));
+        }
+        if (Input.GetKey(KeyCode.S)) //Move Down
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, speed * -1f));
+        }
+	}
+}
