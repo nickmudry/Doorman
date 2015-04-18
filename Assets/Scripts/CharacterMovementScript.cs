b6a27af429d7626b3ef91ff6dd76782796325849
Jacob Peltola <jtpup0@aim.com>
2015-04-18 02:34:46 -0400

    Player Art animation and velocity change

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -3,28 +3,65 @@ using System.Collections;
 
 public class CharacterMovementScript : MonoBehaviour {
     public float speed;
+    public int doorsHeld = 10;
+    public GameObject playerSprite;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKey(KeyCode.D)) //Move Right
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D)) //Move Right
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(speed, 0f));
+            playerSprite.transform.rotation = Quaternion.Euler(65, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.right * speed);
+
+
         }
         if (Input.GetKey(KeyCode.A)) //Move Left
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(speed * -1f, 0f));
+            //hacky rotate
+            playerSprite.transform.rotation = Quaternion.Euler(297, 207, 335);
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * speed);
+
+
         }
         if (Input.GetKey(KeyCode.W)) //Move Up
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, speed));
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+
         }
         if (Input.GetKey(KeyCode.S)) //Move Down
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, speed * -1f));
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * speed);
         }
-	}
+        //checks if moving animate
+        if (GetComponent<Rigidbody>().velocity.magnitude > 1)
+        {
+            if (doorsHeld > 1)
+            {
+                playerSprite.GetComponent<Animator>().SetInteger("Stance", 1);
+            }
+            else
+            {
+                playerSprite.GetComponent<Animator>().SetInteger("Stance", 3);
+            }
+        }
+            //if not moving idle
+        else if (doorsHeld > 1 && GetComponent<Rigidbody>().velocity.magnitude < 1)
+        {
+            playerSprite.GetComponent<Animator>().SetInteger("Stance", 0);
+        }
+        else
+        {
+            playerSprite.GetComponent<Animator>().SetInteger("Stance", 4);
+        }
+    }
+
+    public void doorUsed()
+    {
+        doorsHeld -= 1;
+    }
 }
