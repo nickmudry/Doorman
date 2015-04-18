7a1871a924ed430dc0800fa7dad93f99556256c8
Jacob Peltola <jtpup0@aim.com>
2015-04-18 13:00:44 -0400

    idle and walk, cleaned up playermovement

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -3,79 +3,68 @@ using System.Collections;
 
 public class CharacterMovementScript : MonoBehaviour {
 
-    public float speed = 6.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
+    public GameObject playerModel;
+
+    public float speed;
+    public float gravity;
 
     public int doorsHeld = 10;
-    public GameObject playerModel;
-	// Use this for initialization
-	void Start () {
+
+    private Vector3 moveDirection = Vector3.zero;
 	
-	}
-	
+    void Start()
+    {
+        //start by idle
+        playerModel.GetComponent<Animator>().SetInteger("Stance", 0);
+    }
+
 	// Update is called once per frame
     void Update()
     {
-        //hacky rotate
-        if (Input.GetKey(KeyCode.D)) //Move Right
-        {
-            playerModel.transform.rotation = Quaternion.Euler(0, 180, 0);
-            //gameObject.GetComponent<Rigidbody>().velocity = (transform.right * speed);
-
-
-        }
-        if (Input.GetKey(KeyCode.A)) //Move Left
-        {
-            
-            playerModel.transform.rotation = Quaternion.Euler(0, 0, 0);
-            //gameObject.GetComponent<Rigidbody>().velocity = (-transform.right * speed);
-
-
-        }
-        if (Input.GetKey(KeyCode.W)) //Move Up
-        {
-            playerModel.transform.rotation = Quaternion.Euler(0, 90, 0);
-            //gameObject.GetComponent<Rigidbody>().velocity = (transform.forward * speed);
-
-        }
-        if (Input.GetKey(KeyCode.S)) //Move Down
-        {
-            playerModel.transform.rotation = Quaternion.Euler(0, -90, 0);
-            //gameObject.GetComponent<Rigidbody>().velocity = (-transform.forward * speed);
-        }
-
+        //x and z input
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
 
+        //graity
         moveDirection.y -= gravity * Time.deltaTime;
+
+        //cc magic
         CharacterController cc = GetComponent<CharacterController>();
         cc.Move(moveDirection * Time.deltaTime);
 
-        //checks if moving animate
-        if (GetComponent<Rigidbody>().velocity.magnitude > 1)
+        AnimationChecker();
+        
+}
+    void AnimationChecker()
+    {
+        //animation check with rotation
+        if (Input.GetKey(KeyCode.D)) //Move Right
         {
-            if (doorsHeld > 1)
-            {
-                playerModel.GetComponent<Animator>().SetInteger("Stance", 1);
-            }         
-            else      
-            {         
-                playerModel.GetComponent<Animator>().SetInteger("Stance", 3);
-            }
+            playerModel.GetComponent<Animator>().SetInteger("Stance", 1);
+            playerModel.transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-            //if not moving idle
-        else if (doorsHeld > 1 && GetComponent<Rigidbody>().velocity.magnitude < 1)
+        else if (Input.GetKey(KeyCode.A)) //Move Left
+        {
+            playerModel.GetComponent<Animator>().SetInteger("Stance", 1);
+            playerModel.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.W)) //Move Up
+        {
+            playerModel.GetComponent<Animator>().SetInteger("Stance", 1);
+            playerModel.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.S)) //Move Down
+        {
+            playerModel.GetComponent<Animator>().SetInteger("Stance", 1);
+            playerModel.transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else
         {
             playerModel.GetComponent<Animator>().SetInteger("Stance", 0);
-        }         
-        else      
-        {         
-            playerModel.GetComponent<Animator>().SetInteger("Stance", 4);
         }
     }
-
     public void doorUsed()
     {
         doorsHeld -= 1;
