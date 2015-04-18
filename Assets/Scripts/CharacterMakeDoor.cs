f126825289884d929bea06bae5f0958d630f4d6e
Nick Mudry <nick@mudry.me>
2015-04-18 00:08:17 -0400

    Beginning Of Creation Of Doors

---
+++ Assets/Scripts/CharacterMakeDoor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMakeDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            col.gameObject.SendMessage("CanBecomeDoor", true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            col.gameObject.SendMessage("CanBecomeDoor", false);
+        }
+    }
+}
---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+    public bool canBecomeDoor;
+    public bool becameDoor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Down on " + gameObject.name);
+        if (canBecomeDoor)
+        {
+            becameDoor = true;
+            BecomeDoor();
+        }
+    }
+
+    void BecomeDoor()
+    {
+        becameDoor = true;
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;        
+    }
+
+    void CanBecomeDoor(bool canBeDoor)
+    {
+        canBecomeDoor = canBeDoor;
+    }
+}
