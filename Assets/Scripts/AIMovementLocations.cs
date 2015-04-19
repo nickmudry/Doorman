f8153fca030c954c129758b46c5954f3a3c80cb6
Nick Mudry <nick@mudry.me>
2015-04-18 22:04:49 -0400

    Commiting Before Computer Goes Boom!

---
+++ Assets/Scripts/AIMovementLocations.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovementLocations : MonoBehaviour {
+    public Transform[] nodeLocations;
+    int currentLocation;
+	// Use this for initialization
+	void Start () {
+        currentLocation = Random.Range(0, nodeLocations.Length);
+        gameObject.SendMessage("SetTarget", nodeLocations[currentLocation]);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (gameObject.transform == nodeLocations[currentLocation])
+        {
+            Debug.Log("Reached Location!");
+            currentLocation = Random.Range(0, nodeLocations.Length);
+            gameObject.SendMessage("SetTarget", nodeLocations[currentLocation]);
+        }
+	}
+
+    public void FindNewTarget()
+    {
+        Debug.Log("Reached Location!");
+        currentLocation = Random.Range(0, nodeLocations.Length);
+        gameObject.SendMessage("SetTarget", nodeLocations[currentLocation]);
+    }
+}
---
+++ Assets/Scripts/AINodeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AINodeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter()
+    {
+        //Do some stuff regarding switching the location here! TODO: <<<THAT! 
+    }
+}
